6f6c811e45ae2ab99d64912df1a42ac3261e497a
Tira <cosmicsam1010@yahoo.com>
2017-11-16 20:29:58 -0800

    Initial commit

---
+++ app/src/main/java/dev/bingo/a4330/bingo/HomeScreen.java
@@ -0,0 +1,22 @@
+package dev.bingo.a4330.bingo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+   // @Override
+    //public boolean onCreateOptionsMenu(Menu menu){
+
+      //  MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.activity_home_screen.layoutmenu,menu);
+        //return true;
+    //}
+}
