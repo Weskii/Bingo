044de62228dab00d56cc6c6930b5f2e06300415a
lalooooooooo <lailadin96@gmail.com>
2017-11-26 17:45:09 -0600

    added: lists for dog, activities, and appointments, with xmls for each

---
+++ app/src/main/java/dev/bingo/a4330/bingo/appointmentList.java
@@ -0,0 +1,16 @@
+package dev.bingo.a4330.bingo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Laila on 11/26/2017.
+ */
+
+public class appointmentList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_list);
+    }
+}
---
+++ app/src/main/java/dev/bingo/a4330/bingo/dogList.java
@@ -0,0 +1,21 @@
+package dev.bingo.a4330.bingo;
+
+/**
+ * Created by Laila on 11/26/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Laila on 11/26/2017.
+ */
+
+public class dogList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_list);
+    }
+}
